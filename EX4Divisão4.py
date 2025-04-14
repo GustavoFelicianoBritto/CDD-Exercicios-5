5219f65fb0a80a0b4c478b5e8842e653e4c0d923
GustavoFelicianoBritto <gustavofelicianoemail@gmail.com>
2025-04-14 16:23:21 -0300

    Ex Senha

---
+++ EX4Divisão4.py
@@ -0,0 +1,11 @@
+
+num1=int(input("Digite o primeiro valor: "))
+num2=int(input("Digite o primeiro valor: "))
+
+while num2 ==0:
+    num2 = int(input("Digite o segundo valor diferente de ZERO: "))
+
+resultado = num1/num2
+
+
+print(f"{num1} / {num2} = {resultado}")
---
+++ EX5Senha.py
@@ -0,0 +1,16 @@
+user = "User00"
+password = "123456"
+error = 3
+
+while error>0:
+    passwordInput = input(f"Olá, {user} Por favor, digite sua senha (tentativas restantes {error}: ")
+    if passwordInput == password:
+        print("Usuário logado")
+        break
+    else:
+        error-=1
+        if error<=0:
+            print("Tentativas excedidas")
+
+
+
